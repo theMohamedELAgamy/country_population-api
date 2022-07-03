deb83cac20cac4795266df6a55c1662aebee7d63
theMohamedELAgamy <mohamedelagame82@gmail.com>
2022-07-03 10:55:42 +0200

    data base created

---
+++ database/migrations/2022_07_03_084442_create_countries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            for($i=1960;$i<2019;$i++){
+                $table->string($i);
+            }
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
